0bb4f06d54205c01fb2eb84a60682f457728093b
lauren <laurenzolivier@gmail.com>
2020-10-06 17:22:58 +0200

    Add graphql @ apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Ninja's Reading List</h1>
+      </div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
