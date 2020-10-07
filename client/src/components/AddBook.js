5d3b1215c17b17370d0cded026d000d0ab4ccb00
lauren <laurenzolivier@gmail.com>
2020-10-07 15:28:10 +0200

    AddBook Query & component

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,28 +1,56 @@
 import React, { useState } from 'react';
-import { useQuery } from '@apollo/client';
-import { getAuthorsQuery } from '../queries/queries';
+import { useQuery, useMutation } from '@apollo/client';
+import {
+  getAuthorsQuery,
+  addBookMutation,
+  getBooksQuery,
+} from '../queries/queries';
 
 export default function AddBook() {
+  const [newBook, setNewBook] = useState({
+    name: '',
+    genre: '',
+    authorId: '',
+  });
+
   const { loading, error, data } = useQuery(getAuthorsQuery);
+
+  const [addNewBook] = useMutation(addBookMutation, {
+    variables: {
+      name: newBook.name,
+      genre: newBook.genre,
+      authorId: newBook.authorId,
+    },
+    refetchQueries: [{ query: getBooksQuery }],
+  });
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error here : {error.message}</p>;
   const { authors } = data;
-  console.log(authors);
 
+  const updateBookInfos = e => {
+    setNewBook({ ...newBook, [e.target.name]: e.target.value });
+  };
+
+  const addBook = e => {
+    e.preventDefault();
+    addNewBook();
+    setNewBook({ name: '', genre: '', authorId: '' });
+  };
 
   return (
-    <form id="add-book">
+    <form id="add-book" onSubmit={addBook}>
       <div className="field">
         <label>Book Name:</label>
-        <input type="text"></input>
+        <input type="text" name="name" onChange={updateBookInfos}></input>
       </div>
       <div className="field">
         <label>Genre:</label>
-        <input type="text"></input>
+        <input type="text" name="genre" onChange={updateBookInfos}></input>
       </div>
       <div className="field">
         <label>Select Author:</label>
-        <select name="author">
+        <select name="authorId" onChange={updateBookInfos}>
           <option value="">Please select an author</option>
           {authors.map(author => {
             return (
@@ -33,6 +61,7 @@ export default function AddBook() {
           })}
         </select>
       </div>
+
       <button type="submit">Add</button>
     </form>
   );
--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -20,4 +20,12 @@ const getAuthorsQuery = gql`
   }
 `;
 
-export { getBooksQuery, getAuthorsQuery };
+const addBookMutation = gql`
+  mutation AddBook($name: String!, $genre: String!, $authorId: ID!) {
+    addBook(name: $name, genre: $genre, authorId: $authorId) {
+      name
+      genre
+      id
+    }
+  }
+`;
